eb8b949ca0560f1a1ec5d849fcb38f1ab97d4513
Julián Idiart <julianidiart1987@hotmail.com>
2019-11-04 18:44:38 +0100

    Chapter 3: Binary search [corrected n value in Recursive Binary Search]

--- 3. Algorithms for Sorting and Searching/3.2. Recursive Binary Search/recursiveBinarySearch.js
+++ 3. Algorithms for Sorting and Searching/3.2. Recursive Binary Search/recursiveBinarySearch.js
@@ -1,9 +1,9 @@
 // RECURSIVE-BINARY-SEARCH
-const recursiveBinarySearch = (A, r, x, p = 0) => {
+const recursiveBinarySearch = (A, r, x, p = 1) => {
   if (p > r) return null;
   const q = Math.floor((p + r) / 2);
-  if (A[q] === x) return q;
-  if (A[q] > x) {
+  if (A[q - 1] === x) return q - 1;
+  if (A[q - 1] > x) {
     return recursiveBinarySearch(A, q - 1, x, p);
   } else {
     return recursiveBinarySearch(A, r, x, q + 1);
@@ -22,9 +22,8 @@ const authors = [
   "Terry Pratchett"
 ];
 const tests =
-  recursiveBinarySearch(authors, authors.length - 1, "Aldous Huxley") === 0 &&
-  recursiveBinarySearch(authors, authors.length - 1, "Isaac Asimov") === 3 &&
-  recursiveBinarySearch(authors, authors.length - 1, "Terry Pratchett") === 7 &&
-  recursiveBinarySearch(authors, authors.length - 1, "Jorge Luis Borges") ===
-    null;
+  recursiveBinarySearch(authors, authors.length, "Aldous Huxley") === 0 &&
+  recursiveBinarySearch(authors, authors.length, "Isaac Asimov") === 3 &&
+  recursiveBinarySearch(authors, authors.length, "Terry Pratchett") === 7 &&
+  recursiveBinarySearch(authors, authors.length, "Jorge Luis Borges") === null;
 console.log("recursiveBinarySearch: TESTS " + (tests ? "PASSED" : "FAILED"));
