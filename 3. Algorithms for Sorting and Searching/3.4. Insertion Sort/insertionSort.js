15ae6d7f6a89ebd133e11702851b3d7dfad06190
Julián Idiart <julianidiart1987@hotmail.com>
2019-11-10 19:08:36 +0100

    Chapter 3: Insertion sort [n as default parameter]

--- 3. Algorithms for Sorting and Searching/3.4. Insertion Sort/insertionSort.js
+++ 3. Algorithms for Sorting and Searching/3.4. Insertion Sort/insertionSort.js
@@ -1,5 +1,5 @@
 // INSERTION-SORT
-const insertionSort = (A, n) => {
+const insertionSort = (A, n = authors.length) => {
   for (let i = 1; i < n; i++) {
     let key = A[i],
       j = i - 1;
@@ -36,8 +36,6 @@ const sortedAuthors = [
 const numbers = [9, 20, 1, 5, 7, 32, -4, 15];
 const sortedNumbers = [-4, 1, 5, 7, 9, 15, 20, 32];
 const tests =
-  JSON.stringify(insertionSort(authors, authors.length)) ===
-    JSON.stringify(sortedAuthors) &&
-  JSON.stringify(insertionSort(numbers, numbers.length)) ===
-    JSON.stringify(sortedNumbers);
+  JSON.stringify(insertionSort(authors)) === JSON.stringify(sortedAuthors) &&
+  JSON.stringify(insertionSort(numbers)) === JSON.stringify(sortedNumbers);
 console.log("insertionSort: TESTS " + (tests ? "PASSED" : "FAILED"));
