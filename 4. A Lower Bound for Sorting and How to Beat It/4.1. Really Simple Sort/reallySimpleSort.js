74b8b4aaa76f449c5df56696dfab46fd8f092e66
Julián Idiart <julianidiart1987@hotmail.com>
2019-11-12 00:45:47 +0100

    Chapter 4: Count keys equal

--- 4. A Lower Bound for Sorting and How to Beat It/4.1. Really Simple Sort/reallySimpleSort.js
+++ 4. A Lower Bound for Sorting and How to Beat It/4.1. Really Simple Sort/reallySimpleSort.js
@@ -8,9 +8,8 @@ const reallySimpleSory = (A, n = A.length) => {
 };
 
 // TESTS
-const unsortedArray = [1, 2, 1, 2, 2, 2, 1, 1, 2, 1, 2];
-const sortedArray = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2];
+const numbers = [1, 2, 1, 2, 2, 2, 1, 1, 2, 1, 2];
+const sortedNumbers = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2];
 const tests =
-  JSON.stringify(reallySimpleSory(unsortedArray)) ===
-  JSON.stringify(sortedArray);
+  JSON.stringify(reallySimpleSory(numbers)) === JSON.stringify(sortedNumbers);
 console.log("reallySimpleSory: TESTS " + (tests ? "PASSED" : "FAILED"));
---
+++ 4. A Lower Bound for Sorting and How to Beat It/4.2. Count Keys Equal/countKeysEqual.js
@@ -0,0 +1,16 @@
+// COUNT-KEYS-EQUAL
+const countKeysEqual = (A, n = A.length, m = [...new Set(A)]) => {
+  let equal = m.fill(0);
+  for (let i = 0; i < n; i++) {
+    const key = A[i];
+    equal[key - 1]++;
+  }
+  return equal;
+};
+
+// TESTS
+const numbers = [1, 2, 1, 2, 2, 2, 1, 1, 2, 1, 2];
+const countNumbers = [5, 6];
+const tests =
+  JSON.stringify(countKeysEqual(numbers)) === JSON.stringify(countNumbers);
+console.log("countKeysEqual: TESTS " + (tests ? "PASSED" : "FAILED"));
