8b8400e2fbaaec8180de85e32c6188b4beb11a61
Julián Idiart <julianidiart1987@hotmail.com>
2019-11-12 01:51:27 +0100

    Chapter 4: Count keys equal

--- 4. A Lower Bound for Sorting and How to Beat It/4.2. Count Keys Equal/countKeysEqual.js
+++ 4. A Lower Bound for Sorting and How to Beat It/4.2. Count Keys Equal/countKeysEqual.js
@@ -1,6 +1,6 @@
 // COUNT-KEYS-EQUAL
-const countKeysEqual = (A, n = A.length, m = Math.max(...A)) => {
-  let equal = Array(m + 1).fill(0);
+const countKeysEqual = (A, n = A.length, m = Math.max(...A) + 1) => {
+  let equal = Array(m).fill(0);
   for (let i = 0; i < n; i++) {
     const key = A[i];
     equal[key]++;
