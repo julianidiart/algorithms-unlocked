75b0f67e474017ec1e21b7141f5c5ec77fabc6ed
Julián Idiart <julianidiart1987@hotmail.com>
2019-11-12 01:49:03 +0100

    Chapter 4: Count keys less

---
+++ 4. A Lower Bound for Sorting and How to Beat It/4.3. Count Keys Less/countKeysLess.js
@@ -0,0 +1,18 @@
+// COUNT-KEYS-LESS
+const countKeysLess = (equal, m = equal.length - 1) => {
+  let less = Array(m + 1);
+  less[0] = 0;
+  for (let j = 1; j <= m; j++) {
+    less[j] = less[j - 1] + equal[j - 1];
+  }
+  return less;
+};
+
+// TESTS
+const countKeysEqual = require("../4.2. Count Keys Equal/countKeysEqual");
+const numbers = [4, 1, 0, 1, 2, 4, 8, 6, 3, 9, 9, 1];
+const countNumbers = countKeysEqual(numbers);
+const lessNumbers = [0, 1, 4, 5, 6, 8, 8, 9, 9, 10];
+const tests =
+  JSON.stringify(countKeysLess(countNumbers)) === JSON.stringify(lessNumbers);
+console.log("countKeysLess: TESTS " + (tests ? "PASSED" : "FAILED"));
