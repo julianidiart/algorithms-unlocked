52bb92ce44b0a85fb977bc6ec9bc8fc8bc344230
Julián Idiart <julianidiart1987@hotmail.com>
2019-11-19 20:24:59 +0100

    Chapter 2: Better linear search [jest tests]

--- 2. How to Describe and Evaluate Computer Algorithms/2.2. Better Linear Search/betterLinearSearch.js
+++ 2. How to Describe and Evaluate Computer Algorithms/2.2. Better Linear Search/betterLinearSearch.js
@@ -1,5 +1,6 @@
 // BETTER-LINEAR-SEARCH
-const betterLinearSearch = (A, x, n = A.length) => {
+const betterLinearSearch = (A, x) => {
+  const n = A.length;
   for (let i = 0; i < n; i++) {
     if (A[i] === x) return i;
   }
